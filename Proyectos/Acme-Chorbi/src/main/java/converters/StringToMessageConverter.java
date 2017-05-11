f9ff67b9e6b005df1e05ecf6aa5548bb67a63abc
Jultrocor <j.troncosocorrea@gmail.com>
2017-05-11 13:28:57 +0200

    Configuración de xmls

---
+++ Proyectos/Acme-Chorbi/src/main/java/converters/StringToMessageConverter.java
@@ -0,0 +1,32 @@
+
+package converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class StringToMessageConverter implements Converter<String, Message> {
+
+	@Autowired
+	MessageRepository	messageRepository;
+
+
+	@Override
+	public Message convert(final String text) {
+		Message result;
+		int id;
+
+		try {
+			id = Integer.valueOf(text);
+			result = this.messageRepository.findOne(id);
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+
+		return result;
+	}
+
+}
