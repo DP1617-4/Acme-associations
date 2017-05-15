d2aabd950a90a095de3ac85abe6a7b1357eac439
Alberto <alberto0o095bm@gmail.com>
2017-05-15 13:36:47 +0200

    sistema desplegable para pruebas.

---
+++ Proyectos/Acme-Associations/src/main/java/converters/StringToMeetingConverter.java
@@ -0,0 +1,35 @@
+
+package converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import repositories.MeetingRepository;
+import domain.Meeting;
+
+@Component
+@Transactional
+public class StringToMeetingConverter implements Converter<String, Meeting> {
+
+	@Autowired
+	MeetingRepository	meetingRepository;
+
+
+	@Override
+	public Meeting convert(final String text) {
+		Meeting result;
+		int id;
+
+		try {
+			id = Integer.valueOf(text);
+			result = this.meetingRepository.findOne(id);
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+
+		return result;
+	}
+
+}
