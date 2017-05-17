acd63a34598aca0383ff6e90ec7a21fdba6c3977
Álvaro Martín <amartingordillo@gmail.com>
2017-05-17 21:33:43 +0200

    A+ de mensaje programado en el tiempo

--- Proyectos/Acme-Associations/src/main/java/services/LoanService.java
+++ Proyectos/Acme-Associations/src/main/java/services/LoanService.java
@@ -2,8 +2,10 @@
 package services;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -85,4 +87,11 @@ public class LoanService {
 		return result;
 	}
 
+	public List<Loan> findOverdueLoans(final Pageable page) {
+		List<Loan> result;
+
+		result = this.loanRepository.findOverdueLoans(page);
+
+		return result;
+	}
 }
