958e5ae84a3ae1a561f2ae245fd6d8883773e297
AleGamero <Mastercadena@hotmail.com>
2017-05-19 10:48:30 +0200

    te vas a mori con lo que tiene!!!!

--- Proyectos/Acme-Associations/src/main/java/controllers/user/UserMeetingController.java
+++ Proyectos/Acme-Associations/src/main/java/controllers/user/UserMeetingController.java
@@ -1,8 +1,6 @@
 
 package controllers.user;
 
-import java.util.Collection;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +9,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import services.AssociationService;
 import services.MeetingService;
 import services.RolesService;
-import services.UserService;
 import controllers.AbstractController;
 import domain.Association;
 import domain.Meeting;
-import domain.Roles;
-import domain.User;
 
 @Controller
 @RequestMapping("/user/meeting")
@@ -34,16 +28,10 @@ public class UserMeetingController extends AbstractController {
 
 
 	@Autowired
-	private MeetingService		meetingService;
+	private MeetingService	meetingService;
 
 	@Autowired
-	private AssociationService	associationService;
-
-	@Autowired
-	private RolesService		rolesService;
-
-	@Autowired
-	private UserService			userService;
+	private RolesService	rolesService;
 
 
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
@@ -56,84 +44,49 @@ public class UserMeetingController extends AbstractController {
 	}
 
 	@RequestMapping(value = "/edit", method = RequestMethod.GET)
-	public ModelAndView edit(@RequestParam final int associationId) {
+	public ModelAndView edit(@PathVariable final Meeting meeting, final RedirectAttributes redir) {
 		ModelAndView result;
-		Association association;
-		final User user = this.userService.findByPrincipal();
+		Meeting m;
 
-		association = this.associationService.findOne(associationId);
-		this.rolesService.checkManager(user, association);
+		m = this.meetingService.findOne(meeting.getId());
 
-		result = this.createEditModelAndView(association);
+		try {
+			this.rolesService.checkManagerPrincipal(m.getAssociation());
+			result = this.createEditModelAndView(m);
+		} catch (final Exception e) {
+			result = new ModelAndView("redirect:/welcome/index.do");
+			redir.addFlashAttribute("errorMessage", e.getMessage());
+		}
+
+		result = this.createEditModelAndView(meeting);
 
 		return result;
 	}
-
 	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid Association association, final BindingResult binding) {
+	public ModelAndView save(@Valid Meeting newMeeting, final BindingResult binding) {
 		ModelAndView result;
 
 		if (binding.hasErrors())
-			result = this.createEditModelAndView(association);
+			result = this.createEditModelAndView(newMeeting);
 		else
 			try {
-				final User user = this.userService.findByPrincipal();
-				this.rolesService.checkManager(user, association);
-				association = this.associationService.save(association);
-				result = new ModelAndView("redirect:/association/" + association.getId() + "/display.do");
+				this.rolesService.checkManagerPrincipal(newMeeting.getAssociation());
+				newMeeting = this.meetingService.save(newMeeting);
+				result = new ModelAndView("redirect:/meeting/" + newMeeting.getId() + "/display.do");
 			} catch (final Throwable oops) {
-				result = this.createEditModelAndView(association, "association.commit.error");
+				result = this.createEditModelAndView(newMeeting, "meeting.commit.error");
 			}
 		return result;
 	}
 
-	@RequestMapping(value = "/listOwn", method = RequestMethod.GET)
-	public ModelAndView list() {
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(@PathVariable final Association association) {
 		final ModelAndView result;
 
-		final Collection<Roles> roles = this.rolesService.findAllByPrincipal();
+		this.rolesService.checkCollaboratorPrincipal(association);
 
-		result = new ModelAndView("association/listOwn");
-		result.addObject("roles", roles);
-		result.addObject("requestURI", "/user/association/listOwn.do");
-
-		return result;
-	}
-	//En esta debería haber un mensaje de confirmación al acceder al enlace
-	@RequestMapping(value = "/{association}/leave", method = RequestMethod.GET)
-	public ModelAndView leave(@PathVariable final Association association) {
-		ModelAndView result;
-
-		//Escoja una, hacer query en rolesRepo o hacer el método en association service.
-
-		//		this.associationService.leave(association);
-		final Roles role = this.rolesService.findRolesByPrincipalAssociation(association);
-		this.rolesService.delete(role);
-
-		result = this.list();
-		result.addObject("flashMessage", "association.left");
-
-		return result;
-	}
-
-	@RequestMapping(value = "/{association}/close", method = RequestMethod.GET)
-	public ModelAndView close(@PathVariable final Association association) {
-		ModelAndView result;
-
-		this.associationService.closeAssociationByManager(association.getId());
-
-		result = new ModelAndView("redirect:/association/" + association.getId() + "/display.do");
-
-		return result;
-	}
-
-	@RequestMapping(value = "/{association}/open", method = RequestMethod.GET)
-	public ModelAndView open(@PathVariable final Association association) {
-		ModelAndView result;
-
-		this.associationService.closeAssociationByManager(association.getId());
-
-		result = new ModelAndView("redirect:/association/" + association.getId() + "/display.do");
+		result = new ModelAndView("meeting/list");
+		result.addObject("requestURI", "/user/meeting/list.do");
 
 		return result;
 	}
@@ -153,7 +106,7 @@ public class UserMeetingController extends AbstractController {
 
 		result = new ModelAndView("meeting/edit");
 		result.addObject("meeting", meeting);
-		result.addObject("message", message);
+		result.addObject("errorMessage", message);
 		result.addObject("requestURI", requestURI);
 
 		return result;
